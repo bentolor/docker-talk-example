8573187e60873102a5efee3c0f634877a8b7e619
Ralph Guderlei <ralph.guderlei@gmail.com>
2015-05-26 15:25:39 +0200

    java service powered by dropwizzard

---
+++ java/src/main/java/de/exxcellent/docker/DockerExampleApplication.java
@@ -0,0 +1,43 @@
+package de.exxcellent.docker;
+
+import de.exxcellent.docker.configuration.DockerExampleConfiguration;
+import de.exxcellent.docker.health.TemplateHealthCheck;
+import de.exxcellent.docker.resources.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+/**
+ * Created by rguderlei on 26.05.15.
+ */
+public class DockerExampleApplication extends Application<DockerExampleConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new DockerExampleApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<DockerExampleConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(DockerExampleConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+
+        environment.jersey().register(resource);
+    }
+
+}
